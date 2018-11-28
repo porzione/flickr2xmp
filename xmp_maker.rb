c2589839b2679f21564ae4d6eef90c55b2e3763b
Oleg Kravchenko <okravc@gmail.com>
2018-11-28 11:52:07 +0300

    wip

--- xmp_maker.rb
+++ xmp_maker.rb
@@ -5,23 +5,17 @@
 class XmpMaker
   TK = 'github.com/porzione/flickr2xmp'
 
-  def initialize
+  def initialize(opts)
+    @dry = opts[:dry]
     @tpl = File.read('xmp.erb')
-  end
-
-  def gen(ihsh)
-    ihsh[:title]&.encode!(xml: :text)
-    ihsh[:descr]&.encode!(xml: :text)
-    ihsh[:tags].map! { |t| t.encode xml: :text }
-    ERB.new(@tpl, nil, '-').result_with_hash ihsh.merge(tk: TK)
   end
 
   def write(ihsh, filename)
     content = gen(ihsh)
-    File.open(filename, 'w') { |f| f.write(content) }
-  end
-
-  def str(ihsh)
-    gen(ihsh)
+    ihsh[:title]&.encode!(xml: :text)
+    ihsh[:descr]&.encode!(xml: :text)
+    ihsh[:tags].map! { |t| t.encode xml: :text }
+    content = ERB.new(@tpl, nil, '-').result_with_hash ihsh.merge(tk: TK)
+    @dry ? content : File.open(filename, 'w') { |f| f.write(content) }
   end
 end
